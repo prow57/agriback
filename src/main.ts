898d749d2d3a12a33cb82cde5acea487f8d94c92
Emmanuel Bester <95228455+prow57@users.noreply.github.com>
2024-08-17 21:01:32 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule);
+
+  // Swagger setup
+  const config = new DocumentBuilder()
+    .setTitle('Agri-Assist API')
+    .setDescription('API documentation for the Agri-Assist application')
+    .setVersion('1.0')
+    .addBearerAuth() // Use this if your API uses JWT authentication
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
