9ad6a689eaba6faf93f7975b4947e570e84cb990
Emmanuel Bester <95228455+prow57@users.noreply.github.com>
2024-08-18 01:35:58 +0000

    fixed varEnv

--- agriassistagain/db.js
+++ agriassistagain/db.js
@@ -1,6 +1,6 @@
-var admin = require("firebase-admin");
+const admin = require("firebase-admin");
 
-var serviceAccount = require(process.env.FIREBASE_CONFIG);
+const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
