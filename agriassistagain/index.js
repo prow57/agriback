7bf90fea59110048ab4285cf69b37d2afc2f5c05
Emmanuel Bester <95228455+prow57@users.noreply.github.com>
2024-08-23 21:15:31 +0300

    added Endpoint for auth in index.js

--- agriassistagain/index.js
+++ agriassistagain/index.js
@@ -10,6 +10,7 @@ require('dotenv').config();
 const courseRoutes = require('./src/routes/courseRoutes');
 const adviceRoutes = require('./src/routes/adviceRoutes');
 const chatRoutes = require('./src/routes/chatRoutes');
+const authRoutes = require('./src/routes/authRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -22,6 +23,7 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
+app.use('/api/auth', authRoutes');
 app.use('/api/courses', courseRoutes);
 app.use('/api/advice', adviceRoutes);
 app.use('/api/chat', chatRoutes);
