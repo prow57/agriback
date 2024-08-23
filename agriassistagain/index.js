6687e8bc75402a47a5742ee0d58bc6db1d1a4744
Emmanuel Bester <95228455+prow57@users.noreply.github.com>
2024-08-23 23:19:20 +0300

    Update index.js

--- agriassistagain/index.js
+++ agriassistagain/index.js
@@ -28,7 +28,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/courses', courseRoutes);
 app.use('/api/advice', adviceRoutes);
 app.use('/api/chat', chatRoutes);
-app.use('/api/user',userRoutes);
+app.use('/api/personal', userRoutes);
 
 // Default route
 app.get('/', (req, res) => {
