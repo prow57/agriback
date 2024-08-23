26a78c530e35f448012a791cad562734bf843c23
Emmanuel Bester <95228455+prow57@users.noreply.github.com>
2024-08-23 23:34:55 +0300

    Update index.js

--- agriassistagain/index.js
+++ agriassistagain/index.js
@@ -22,7 +22,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// Routes
+// Routes for Endpoints 
 app.use('/api/auth', authRoutes);
 app.use('/api/courses', courseRoutes);
 app.use('/api/advice', adviceRoutes);
