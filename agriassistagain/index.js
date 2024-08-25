5bb1cb2cd93971201583018e471aafe1178f5b14
Emmanuel Bester <95228455+prow57@users.noreply.github.com>
2024-08-25 23:17:51 +0300

    finally updated index.js

--- agriassistagain/index.js
+++ agriassistagain/index.js
@@ -1,17 +1,4 @@
-// Initialize Firebase Admin SDK
-const admin = require('firebase-admin');
-require('dotenv').config(); // Load environment variables early
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
-
-const db = admin.firestore(); // Initialize Firestore
-
-// Export db so it can be used in other modules
-module.exports = { db };
 
 //index.js
 
