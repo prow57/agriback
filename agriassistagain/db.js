7b5d4e7b1ddad42aba5f4973d8f5357512afada2
Emmanuel Bester <95228455+prow57@users.noreply.github.com>
2024-08-25 20:14:09 +0300

    Updated to fix db.js

--- agriassistagain/db.js
+++ agriassistagain/db.js
@@ -1,14 +1,16 @@
-const admin = require("firebase-admin");
+const admin = require('firebase-admin');
 const { getStorage } = require('firebase-admin/storage');
 
+// Initialize Firebase Admin
 const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  storageBucket: gs://agriassist-a7e77.appspot.com,
+  storageBucket: 'gs://agriassist-a7e77.appspot.com', // Ensure this is a string
 });
 
+// Initialize Firestore and Storage
 const db = admin.firestore();
 const storage = getStorage().bucket();
 
-module.exports = db;
+module.exports = { db, storage };
