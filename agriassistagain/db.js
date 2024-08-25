5532da814c06f68c2ebaa5f7a82e4b4065a094ba
Emmanuel Bester <95228455+prow57@users.noreply.github.com>
2024-08-25 21:06:31 +0300

    Update db.js

--- agriassistagain/db.js
+++ agriassistagain/db.js
@@ -1,15 +1,13 @@
 const admin = require('firebase-admin');
 const { getStorage } = require('firebase-admin/storage');
 
-// Initialize Firebase Admin
 const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  storageBucket: 'gs://agriassist-a7e77.appspot.com', // Ensure this is a string
+  storageBucket: 'agriassist-a7e77.appspot.com',
 });
 
-// Initialize Firestore and Storage
 const db = admin.firestore();
 const storage = getStorage().bucket();
 
