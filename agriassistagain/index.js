6d315c723a75b978d6ce45a59729ec9cf21de548
Emmanuel Bester <95228455+prow57@users.noreply.github.com>
2024-08-25 22:02:27 +0300

    Update index.js

--- agriassistagain/index.js
+++ agriassistagain/index.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const cors = require('cors');
-const { db, storage } = require('firebase-admin');
+const { db, storage } = require('./db');
 
 require('dotenv').config();
 
