a5b106d6dc08653e69f80ab629b00f4cb59904a7
Emmanuel Bester <95228455+prow57@users.noreply.github.com>
2024-08-25 22:55:23 +0300

    Update index.js

--- agriassistagain/index.js
+++ agriassistagain/index.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const cors = require('cors');
-const { db, storage } = require('./db');
+const db= require('firebase-admin);
 
 require('dotenv').config();
 
@@ -11,7 +11,7 @@ const adviceRoutes = require('./src/routes/adviceRoutes');
 const chatRoutes = require('./src/routes/chatRoutes');
 const authRoutes = require('./src/routes/authRoutes');
 const otpRoutes = require('./src/routes/otpRoutes');
-const cropRoutes = require('./src/routes/cropRoutes');
+//const cropRoutes = require('./src/routes/cropRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -26,7 +26,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/courses', courseRoutes);
 app.use('/api/advice', adviceRoutes);
 app.use('/api/chat', chatRoutes);
-app.use('/api/vision', cropRoutes);
+//app.use('/api/vision', cropRoutes);
 
 // Default route
 app.get('/', (req, res) => {
