d99717e892a044c7e4cb58b87c65fa77bec7fa9b
Emmanuel Bester <95228455+prow57@users.noreply.github.com>
2024-09-19 23:03:49 +0200

    Update swagger.js

--- agriassistagain/swagger.js
+++ agriassistagain/swagger.js
@@ -1,1 +1,29 @@
+//swagger.js
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 
+const swaggerDefinition = {
+  openapi: '3.0.0',
+  info: {
+    title: 'Agri-Assist-AI API Documentation', // Title of your API
+    version: '1.0.0',
+    description: 'API documentation for Agri-Assist-AI backend',
+  },
+  servers: [
+    {
+      url: 'https://agriback-plum.vercel.app', // Your API server URL (update if needed)
+      description: 'Development server for Agriassist-AI by Open-TechZ, A2SV Hackathon Semifinalist',
+    },
+  ],
+};
+
+const options = {
+  swaggerDefinition,
+  apis: ['./src/routes/*.js'], // Location of your route files
+};
+
+const swaggerSpec = swaggerJsdoc(options);
+
+module.exports = (app) => {
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+};
